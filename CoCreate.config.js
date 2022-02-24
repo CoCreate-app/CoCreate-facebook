92aeb71f027fe454cfa43d850b4d30ba2102cf2f
Frank <frank@cocreate.app>
2022-02-24 19:10:28 +0000

    fix: CoCreate.config replace CoCreate.app with *

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -14,7 +14,7 @@ module.exports = {
                 "name": "index.html",
                 "path": "/docs/facebook/index.html",
                 "domains": [
-                    "cocreate.app",
+                    "*",
                     "general.cocreate.app"
                 ],
                 "directory": "/docs/facebook",
